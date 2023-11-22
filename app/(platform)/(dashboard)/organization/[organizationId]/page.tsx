933b705397f29476beb1f51eed0a4a42ddb207f9
Trummm <trungpro2000.tp2@gmail.com>
2023-11-22 14:59:19 +0700

    server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,21 @@
-const OrganizationIdPage = () => {
-  return <div>adawd</div>
+import { db } from '@/lib/db'
+import Board from './board'
+import { Form } from './form'
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany()
+
+  return (
+    <div className='flex flex-col space-y-4'>
+      <Form />
+
+      <div className='space-y-2'>
+        {boards.map(({ id, title }) => (
+          <Board key={id} title={title} id={id} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default OrganizationIdPage
